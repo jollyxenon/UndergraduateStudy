65298027934cd75c3c450b86c8767d9703304715
jollyxenon <1378319314@qq.com>
2026-05-31 23:28:40 +0800

    Finish and Archive HW7

--- CS100/Assignment/HW7/src/polynomial.cpp
+++ CS100/Assignment/HW7/src/polynomial.cpp
@@ -1,3 +1,107 @@
 #include "polynomial.hpp"
+#include <fstream>
 
 // YOUR CODE HERE
+Polynomial::Polynomial() : m_coeffs{0.0} {}
+
+Polynomial::Polynomial(const std::vector<double> &coeffs) : m_coeffs(coeffs) {}
+Polynomial::Polynomial(std::vector<double> &&coeffs)
+    : m_coeffs(std::move(coeffs)) {}
+Polynomial::Polynomial(std::initializer_list<double> coeffs)
+    : m_coeffs(coeffs) {}
+
+Polynomial::Polynomial(const std::string &path) {
+  // read coefficients from file
+  std::ifstream infile(path);
+  double coeff;
+  while (infile >> coeff) {
+    m_coeffs.push_back(coeff);
+  }
+}
+
+Polynomial::Polynomial(const Polynomial &p) : m_coeffs(p.m_coeffs) {}
+Polynomial::Polynomial(Polynomial &&p) noexcept
+    : m_coeffs(std::move(p.m_coeffs)) {}
+
+Polynomial &Polynomial::operator=(const Polynomial &p) {
+  if (this != &p) {
+    m_coeffs = p.m_coeffs;
+  }
+  return *this;
+}
+Polynomial &Polynomial::operator=(Polynomial &&p) noexcept {
+  if (this != &p) {
+    m_coeffs = std::move(p.m_coeffs);
+  }
+  return *this;
+}
+
+Polynomial::~Polynomial() {}
+
+double &Polynomial::operator[](int index) { return m_coeffs[index]; }
+const double &Polynomial::operator[](int index) const {
+  return m_coeffs[index];
+}
+
+int Polynomial::size() const { return m_coeffs.size(); }
+
+Polynomial &Polynomial::operator+=(const Polynomial &p) {
+  if (p.size() > size()) {
+    m_coeffs.resize(p.size(), 0.0);
+  }
+  for (int i = 0; i < p.size(); ++i) {
+    m_coeffs[i] += p.m_coeffs[i];
+  }
+  return *this;
+}
+Polynomial Polynomial::operator+(const Polynomial &p) const {
+  Polynomial result(*this);
+  result += p;
+  return result;
+}
+Polynomial &Polynomial::operator-=(const Polynomial &p) {
+  if (p.size() > size()) {
+    m_coeffs.resize(p.size(), 0.0);
+  }
+  for (int i = 0; i < p.size(); ++i) {
+    m_coeffs[i] -= p.m_coeffs[i];
+  }
+  return *this;
+}
+Polynomial Polynomial::operator-(const Polynomial &p) const {
+  Polynomial result(*this);
+  result -= p;
+  return result;
+}
+Polynomial &Polynomial::operator*=(const Polynomial &p) {
+  std::vector<double> result(size() + p.size() - 1, 0.0);
+  for (int i = 0; i < size(); ++i) {
+    for (int j = 0; j < p.size(); ++j) {
+      result[i + j] += m_coeffs[i] * p.m_coeffs[j];
+    }
+  }
+  m_coeffs = std::move(result);
+  return *this;
+}
+Polynomial Polynomial::operator*(const Polynomial &p) const {
+  Polynomial result(*this);
+  result *= p;
+  return result;
+}
+Polynomial Polynomial::operator*(double factor) const {
+  Polynomial result(*this);
+  for (auto &coeff : result.m_coeffs) {
+    coeff *= factor;
+  }
+  return result;
+}
+
+double Polynomial::operator()(double x) const {
+  double result = 0.0;
+  double power = 1.0;
+  for (const auto &coeff : m_coeffs) {
+    result += coeff * power;
+    power *= x;
+  }
+  return result;
+}
--- CS100/Assignment/HW7/src/polynomial_parser.cpp
+++ CS100/Assignment/HW7/src/polynomial_parser.cpp
@@ -1,3 +1,51 @@
 #include "polynomial_parser.hpp"
+#include <sstream>
+#include <stack>
 
 // YOUR CODE HERE
+Polynomial
+PolynomialParser::compute_polynomial(const std::string &expression,
+                                     std::map<std::string, Polynomial> &polys) {
+  std::string temp;
+  std::stringstream ss(expression);
+  std::stack<Polynomial> operators;
+  while (ss >> temp) {
+    if (temp == "+" && operators.size() >= 2) {
+      Polynomial p2 = operators.top();
+      operators.pop();
+      Polynomial p1 = operators.top();
+      operators.pop();
+      operators.push(p1 + p2);
+    } else if (temp == "-" && operators.size() >= 2) {
+      Polynomial p2 = operators.top();
+      operators.pop();
+      Polynomial p1 = operators.top();
+      operators.pop();
+      operators.push(p1 - p2);
+    } else if (temp == "*" && operators.size() >= 2) {
+      Polynomial p2 = operators.top();
+      operators.pop();
+      Polynomial p1 = operators.top();
+      operators.pop();
+      operators.push(p1 * p2);
+    } else if ((temp == "+" || temp == "-" || temp == "*") &&
+               operators.size() < 2) {
+      throw std::invalid_argument(
+          "Invalid expression: not enough operands for operator " + temp);
+    } else {
+      operators.push(polys[temp]);
+    }
+  }
+  if (operators.size() != 1) {
+    throw std::invalid_argument(
+        "Invalid expression: too many operands or operators");
+  }
+  return operators.top();
+}
+
+PolynomialParser::scalarFct
+PolynomialParser::compute_lambda(const std::string &expression,
+                                 std::map<std::string, Polynomial> &polys) {
+  Polynomial p = compute_polynomial(expression, polys);
+  return [p](double x) -> double { return p(x); };
+}
