c77eb3cb07fff933b34568cfb407fd125f619ce0
jollyxenon <1378319314@qq.com>
2026-05-31 22:13:02 +0800

    fix: Format `include/`

--- CS100/Assignment/HW7/include/polynomial_parser.hpp
+++ CS100/Assignment/HW7/include/polynomial_parser.hpp
@@ -2,24 +2,25 @@
 #define CS100_HW7_POLYNOMIALPARSER_HPP_
 
 #include "polynomial.hpp"
-#include <string>
-#include <map>
 #include <functional>
+#include <map>
+#include <string>
 
 class PolynomialParser {
- public:
-  //procedure to compute a polynomial function as a new polynomial
-  //takes a map of polynomials that have been loaded and are used in the function
-  //  (key=name of polynomial)
-  //  (value=the polynomial)
-  Polynomial compute_polynomial( const std::string& expression,
-                                 std::map<std::string,Polynomial>& polys);
+public:
+  // procedure to compute a polynomial function as a new polynomial
+  // takes a map of polynomials that have been loaded and are used in the
+  // function
+  //   (key=name of polynomial)
+  //   (value=the polynomial)
+  Polynomial compute_polynomial(const std::string &expression,
+                                std::map<std::string, Polynomial> &polys);
 
-  //procedure to compute a lambda function that represents the polynomial function
-  //parameters are same than for the previous function
+  // procedure to compute a lambda function that represents the polynomial
+  // function parameters are same than for the previous function
   typedef std::function<double(double)> scalarFct;
-  scalarFct compute_lambda( const std::string& expression,
-                            std::map<std::string, Polynomial> & polys);
+  scalarFct compute_lambda(const std::string &expression,
+                           std::map<std::string, Polynomial> &polys);
 };
 
 #endif
