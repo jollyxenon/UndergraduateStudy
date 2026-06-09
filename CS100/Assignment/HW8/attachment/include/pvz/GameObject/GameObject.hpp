9f527fa7f3fd1d45bf53d91b8bb0df5cecc007d5
jollyxenon <1378319314@qq.com>
2026-06-09 11:18:08 +0800

    Init CS100 HW8

---
+++ CS100/Assignment/HW8/attachment/include/pvz/GameObject/GameObject.hpp
@@ -0,0 +1,16 @@
+#ifndef GAMEOBJECT_HPP__
+#define GAMEOBJECT_HPP__
+
+#include "pvz/Framework/ObjectBase.hpp"
+
+// Declares the class name GameWorld so that its pointers can be used.
+class GameWorld;
+
+class GameObject : public ObjectBase {
+public:
+
+private:
+
+};
+
+#endif // !GAMEOBJECT_HPP__
---
+++ CS100/Assignment/HW8/attachment/src/GameWorld/GameWorld.cpp
@@ -0,0 +1,14 @@
+#include "pvz/GameWorld/GameWorld.hpp"
+
+void GameWorld::Init() {
+  // YOUR CODE HERE
+}
+
+LevelStatus GameWorld::Update() {
+  // YOUR CODE HERE
+  return LevelStatus::ONGOING;
+}
+
+void GameWorld::CleanUp() {
+  // YOUR CODE HERE
+}
